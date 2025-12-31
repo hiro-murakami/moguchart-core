21fa192076e345bc33d8cc406d9a914bbafe5fa5
Murakami Hiroyuki <hiroyuki.m@gmail.com>
2025-12-31 15:52:51 +0900

    Vercelで公開できるようにしたい

--- vite.config.ts
+++ vite.config.ts
@@ -7,25 +7,31 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    lib: {
-      // エントリーポイント（index.d.tsの元になるファイル）
-      entry: 'src/index.ts',
-      // ライブラリ名（UMD形式などで使用されます）
-      name: 'Moguchart',
-      // 出力されるファイル名のベース
-      fileName: 'moguchart',
-      // 出力形式
-      formats: ['es', 'umd'],
-    },
-    rollupOptions: {
-      // ライブラリに含めたくない依存関係（litなど）を指定
-      external: ['lit'],
-      output: {
-        globals: {
-          lit: 'Lit',
+  // mode が 'demo' の場合はアプリとしてビルド、それ以外はライブラリとしてビルド
+  build:
+    process.env.BUILD_MODE === 'demo'
+      ? {
+          outDir: 'dist-demo', // デモ用の出力先
+        }
+      : {
+          lib: {
+            // エントリーポイント（index.d.tsの元になるファイル）
+            entry: 'src/index.ts',
+            // ライブラリ名（UMD形式などで使用されます）
+            name: 'Moguchart',
+            // 出力されるファイル名のベース
+            fileName: 'moguchart',
+            // 出力形式
+            formats: ['es', 'umd'],
+          },
+          rollupOptions: {
+            // ライブラリに含めたくない依存関係（litなど）を指定
+            external: ['lit'],
+            output: {
+              globals: {
+                lit: 'Lit',
+              },
+            },
+          },
         },
-      },
-    },
-  },
 })
