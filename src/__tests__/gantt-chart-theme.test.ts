9dfed4bfec3ae4febecd3bbff5f49208dd11d3b1
Murakami Hiroyuki <hiroyuki.m@gmail.com>
2026-02-08 17:11:06 +0900

    test: Use globalThis for ResizeObserver mock in tests

--- src/__tests__/gantt-chart-theme.test.ts
+++ src/__tests__/gantt-chart-theme.test.ts
@@ -10,7 +10,7 @@ Object.defineProperty(window, 'matchMedia', {
 })
 
 // ResizeObserverのモック
-global.ResizeObserver = class ResizeObserver {
+globalThis.ResizeObserver = class ResizeObserver {
   observe() {}
   unobserve() {}
   disconnect() {}
