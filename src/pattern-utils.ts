d45954db0f444448590d319b9473563f31337e5b
Murakami Hiroyuki <hiroyuki.m@gmail.com>
2026-02-11 15:04:02 +0900

    fix: Explicitly set `background-repeat: repeat;` for all generated background patterns.

--- src/pattern-utils.ts
+++ src/pattern-utils.ts
@@ -12,70 +12,83 @@ export function getPatternStyle(pattern?: GanttTaskPattern): string {
       return `
           background-image: linear-gradient(45deg, ${color} 25%, transparent 25%, transparent 50%, ${color} 50%, ${color} 75%, transparent 75%, transparent);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'diagonal-stripe-thin':
       size = '4px'
       return `
           background-image: linear-gradient(45deg, ${color} 25%, transparent 25%, transparent 50%, ${color} 50%, ${color} 75%, transparent 75%, transparent);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'diagonal-stripe-thick':
       size = '16px'
       return `
           background-image: linear-gradient(45deg, ${color} 25%, transparent 25%, transparent 50%, ${color} 50%, ${color} 75%, transparent 75%, transparent);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'diagonal-stripe-reverse':
       return `
           background-image: linear-gradient(135deg, ${color} 25%, transparent 25%, transparent 50%, ${color} 50%, ${color} 75%, transparent 75%, transparent);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'diagonal-grid':
       return `
           background-image: linear-gradient(45deg, ${color} 25%, transparent 25%, transparent 50%, ${color} 50%, ${color} 75%, transparent 75%, transparent), linear-gradient(135deg, ${color} 25%, transparent 25%, transparent 50%, ${color} 50%, ${color} 75%, transparent 75%, transparent);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'vertical-stripe':
       return `
           background-image: linear-gradient(90deg, ${color} 50%, transparent 50%);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'horizontal-stripe':
       return `
           background-image: linear-gradient(0deg, ${color} 50%, transparent 50%);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'checkerboard':
       return `
           background-image: linear-gradient(45deg, ${color} 25%, transparent 25%, transparent 75%, ${color} 75%, ${color}), linear-gradient(45deg, ${color} 25%, transparent 25%, transparent 75%, ${color} 75%, ${color});
           background-position: 0 0, calc(${size} / 2) calc(${size} / 2);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'dots':
       return `
           background-image: radial-gradient(${color} 20%, transparent 20%);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'dots-dense':
       size = '4px'
       return `
           background-image: radial-gradient(${color} 20%, transparent 20%);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'triangle':
       return `
           background-image: conic-gradient(from 150deg at 50% 35%, ${color} 60deg, transparent 60deg);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'circle':
       return `
           background-image: radial-gradient(circle, transparent 50%, ${color} 50%, ${color} 70%, transparent 70%);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     case 'grid':
       return `
           background-image: linear-gradient(${color} 1px, transparent 1px), linear-gradient(90deg, ${color} 1px, transparent 1px);
           background-size: ${size} ${size};
+          background-repeat: repeat;
         `
     default:
       return ''
